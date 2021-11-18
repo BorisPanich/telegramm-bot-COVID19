5aab0079962aa2c48742ab892e1e8f0ea1c1b6c1
BorisPanich <panich2303@gmail.com>
2021-11-18 05:59:10 +0300

    added button with countries

--- bot.js
+++ bot.js
@@ -1,9 +1,24 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
 const api = require('covid19-api')
+const Markup = require('telegraf/markup')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply(`Welcome ${ctx.message.from.first_name}`))
+bot.start((ctx) => ctx.reply(`Welcome ${ctx.message.from.first_name}!
+You know a statistic of COVID19.
+Input a name of country from English and get a statistic.
+Looking all list of countries can command /help.
+`,
+    Markup.keyboard([
+        ['Belarus', 'Russia'],
+        ['Ukraine', 'Poland'],
+        ['Lethuania', 'Latvia']
+    ])
+        .resize()   // нормализует размер кнопок
+        .extra()    // выводит кнопки
+)
+)
+
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('text', async (ctx) => {
     let data = {}
